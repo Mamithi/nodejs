b1c2aaa44c78e19aaefd3fa520fbb1db2c8e5ab3
Mamithi <19lawrence93@gmail.com>
2018-09-15 13:15:23 +0300

    Create a REST CRUD with genre

--- express-demo/index.js
+++ express-demo/index.js
@@ -24,8 +24,7 @@ app.post('/api/courses', (req, res) => {
 
     if(error){
         // 400 bad reques
-        res.status(400).send(error.details[0].message);
-        return;
+        return res.status(400).send(error.details[0].message);
     }
 
     const course = {
@@ -42,7 +41,7 @@ app.get('/api/courses/:id', (req, res) => {
     const course = courses.find( c => c.id === parseInt(req.params.id));
 
     if(!course){
-        res.status(404).send(`The course with id ${req.params.id} not found`);
+        return res.status(404).send(`The course with id ${req.params.id} not found`);
     }
 
     res.send(course);
@@ -53,21 +52,34 @@ app.put('/api/courses/:id', (req, res) => {
     const course = courses.find( c => c.id === parseInt(req.params.id));
 
     if(!course){
-        res.status(404).send(`The course with id ${req.params.id} not found`);
+       return  res.status(404).send(`The course with id ${req.params.id} not found`);
     }
 
     const { error } = validateCourse(req.body);  //result.error 
 
     if(error){
         // 400 bad reques
-        res.status(400).send(error.details[0].message);
-        return;
+        return res.status(400).send(error.details[0].message);
     }
 
     course.name = req.body.name;
 
     res.send(course);
 
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find( c => c.id === parseInt(req.params.id));
+
+    if(!course){
+       return res.status(404).send(`The course with id ${req.params.id} not found`);
+    }
+
+   const index = courses.indexOf(course);
+   courses.splice(index, 1);
+
+   res.send(course);
+//    return;
 });
 
 function validateCourse(course){
