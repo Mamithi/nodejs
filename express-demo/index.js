acf73ce4f95b587bc2eff9e3f9d3eca41a80175e
Mamithi <19lawrence93@gmail.com>
2018-09-15 21:50:04 +0300

    Add env

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const Joi = require('joi');
+const helmet = require('helmet')
+const morgan = require('morgan')
 const logger = require('./logger');
 const auth = require('./auth');
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded( {extended: true} ));
+app.use(express.static('public'))
+app.use(helmet());
 
-app.use(logger);
-app.use(auth);
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled')
+}
+
+// app.use(logger);
+// app.use(auth);
 
 
 const courses = [
