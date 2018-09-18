64226438d79107bf5f8ce3fbe979704549acf4e7
Mamithi <19lawrence93@gmail.com>
2018-09-18 18:13:13 +0300

    Restructuring code

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,110 +1,29 @@
 const express = require('express');
 const Joi = require('joi');
-const helmet = require('helmet')
-const morgan = require('morgan')
-const logger = require('./logger');
-const auth = require('./auth');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const config = require('config');
+const debug = require('debug')('app:startup');
+const logger = require('./middleware/logger');
+
+const courses = require('./routes/courses');
+const home = require('./routes/home');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.use(express.json());
 app.use(express.urlencoded( {extended: true} ));
 app.use(express.static('public'))
 app.use(helmet());
+app.use('/api/courses', courses);
+app.use('/', home);
 
 if(app.get('env') === 'development'){
     app.use(morgan('tiny'));
-    console.log('Morgan enabled')
-}
-
-// app.use(logger);
-// app.use(auth);
-
-
-const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'},
-];
-
-app.get('/', (req, res) => {
-    res.send('Hello World!!!');   
-});
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-});
-
-app.post('/api/courses', (req, res) => {
-    const { error } = validateCourse(req.body);  //result.error 
-
-    if(error){
-        // 400 bad reques
-        return res.status(400).send(error.details[0].message);
-    }
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name,
-    };
-
-    courses.push(course);
-
-    res.send(course);
-});
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find( c => c.id === parseInt(req.params.id));
-
-    if(!course){
-        return res.status(404).send(`The course with id ${req.params.id} not found`);
-    }
-
-    res.send(course);
-});
-
-
-app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find( c => c.id === parseInt(req.params.id));
-
-    if(!course){
-       return  res.status(404).send(`The course with id ${req.params.id} not found`);
-    }
-
-    const { error } = validateCourse(req.body);  //result.error 
-
-    if(error){
-        // 400 bad reques
-        return res.status(400).send(error.details[0].message);
-    }
-
-    course.name = req.body.name;
-
-    res.send(course);
-
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find( c => c.id === parseInt(req.params.id));
-
-    if(!course){
-       return res.status(404).send(`The course with id ${req.params.id} not found`);
-    }
-
-   const index = courses.indexOf(course);
-   courses.splice(index, 1);
-
-   res.send(course);
-//    return;
-});
-
-function validateCourse(course){
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    return Joi.validate(course, schema);
-
+    debug('Morgan enabled');
 }
 
 // PORT
--- vidly/index.js
+++ vidly/index.js
@@ -1,93 +1,13 @@
 const express = require('express');
 const Joi = require('joi');
+const genres = require('./routes/genres');
+const home = require('./routes/home');
 
 const app = express();
 
 app.use(express.json());
-
-const genres = [
-    { id: 1, category: 'Horror'},
-    { id: 2, category: 'Comedy'},
-    { id: 3, category: 'Romantic'},
-    { id: 4, category: 'History'},
-    { id: 5, category: 'Fiction'},
-];
-
-app.get('/', (req, res) => {
-    res.send('Hello world');
-})
-
-app.get('/api/genres', (req, res) => {
-    return res.send(genres);
-});
-
-app.get('/api/genres/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id))
-
-    if(!genre){
-        return res.status(404).send(`Video genre with id ${req.params.id} not found`);
-    }
-
-    return res.send(genre);
-});
-
-app.post('/api/genres', (req, res) => {
-    const { error } = validateGenres(req.body);
-
-    if(error){
-        return res.status(400).send(error.details[0].message);
-    }
-
-    const genre = {
-        id : genres.length + 1,
-        category : req.body.category
-    };
-
-    genres.push(genre);
-
-    return res.status(201).send(genre);
-});
-
-app.put('/api/genres/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id))
-
-    if(!genre){
-        return res.status(404).send(`Video genre with id ${req.params.id} not found`);
-    }
-
-    const { error } = validateGenres(req.body);
-
-    if(error){
-        return res.status(400).send(error.details[0].message);
-    }
-
-    genre.category = req.body.category;
-
-    res.status(200).send(genre);
-
-});
-
-app.delete('/api/genres/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id))
-
-    if(!genre){
-        return res.status(404).send(`Video genre with id ${req.params.id} not found`);
-    }
-
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
-
-    return res.send(genre);
-});
-
-function validateGenres(genre) {
-    const schema = {
-        category : Joi.string().min(3).required()
-    };
-
-    return Joi.validate(genre, schema)
-
-}
+app.use('/api/genres', genres);
+app.use('/', home);
 
 const port = process.env.PORT || 3000;
 
