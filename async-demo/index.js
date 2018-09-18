15085f72cc8df754c16d8dc2752258f498a9a038
Mamithi <19lawrence93@gmail.com>
2018-09-18 19:08:11 +0300

    Promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,12 +1,22 @@
 console.log('Before');
-getUser(1, (user) => {
-    console.log('User', user)
-
-    getRepositories(user.username, (repos) => {
-        console.log('Repositories', repos);
-    })
-});
+getUser(1, displayUsers);
 console.log('After');
+
+
+function displayUsers(user){
+    console.log('User', user)
+    getRepositories(user.username, displayRepos);
+}
+function displayRepos(repos) {
+    console.log('Repositories', repos);
+        getCommits(repos[0], displayCommits);
+}
+
+function displayCommits(commits){
+    console.log(commits);
+}
+
+
 
 function getUser(id, callback){
     setTimeout(() => {
@@ -23,6 +33,14 @@ function getRepositories(username, callback) {
     setTimeout(() => {
         console.log(`Fetching ${username} github repositories....`);
         callback(['repo1', 'repo2', 'repo3'])
+
+    }, 2000);
+}
+
+function getCommits(repo, callback) {
+    setTimeout(() => {
+        console.log(`Fetching ${repo} github repositories....`);
+        callback(['message1', 'message2', 'message3'])
 
     }, 2000);
 }
---
+++ async-demo/promise.js
@@ -0,0 +1,11 @@
+const p = new Promise((resolve, reject) => {
+    // Start some async work
+    // ...
+    setTimeout(() => {
+        // resolve(1);
+        reject(new Error('This is the error message'));
+    }, 2000);    
+});
+
+p.then(result => console.log('Result', result))
+.catch(err => console.log('Error', err.message));
