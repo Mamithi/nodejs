d7eea9845e0f66df9edbca2c24fac5c4d3c5cb2c
Mamithi <19lawrence93@gmail.com>
2018-09-18 19:23:54 +0300

    Use promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,46 +1,49 @@
 console.log('Before');
-getUser(1, displayUsers);
+getUser(1, (user) => {
+    console.log('User', user)
+    getRepositories(user.username, (repos) => {
+        console.log('Repositories', repos);
+        getCommits(repos[0], (commits) => {
+            console.log('Commit Messages', commits);
+        })
+    })
+});
 console.log('After');
 
+function getUser(id){
 
-function displayUsers(user){
-    console.log('User', user)
-    getRepositories(user.username, displayRepos);
-}
-function displayRepos(repos) {
-    console.log('Repositories', repos);
-        getCommits(repos[0], displayCommits);
-}
-
-function displayCommits(commits){
-    console.log(commits);
-}
-
-
-
-function getUser(id, callback){
-    setTimeout(() => {
-        console.log('Reading a user from a database...');
-        callback({
-            id: id, 
-            username: 'John Doe'
-        })
-    }, 2000);
+    return new Promise((resolve, reject) => {
+        // Start some async funtion
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({
+                id: id, 
+                username: 'John Doe'
+            })
+        }, 2000);
+    })
 
 }
 
-function getRepositories(username, callback) {
-    setTimeout(() => {
-        console.log(`Fetching ${username} github repositories....`);
-        callback(['repo1', 'repo2', 'repo3'])
+function getRepositories(username) {
 
-    }, 2000);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`Fetching ${username} github repositories....`);
+            resolve(['repo1', 'repo2', 'repo3'])
+    
+        }, 2000);
+    });
+
+    
 }
 
-function getCommits(repo, callback) {
-    setTimeout(() => {
-        console.log(`Fetching ${repo} github repositories....`);
-        callback(['message1', 'message2', 'message3'])
-
-    }, 2000);
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`Fetching commit messages of ${repo}....`);
+            resolve(['message1', 'message2', 'message3'])
+    
+        }, 2000);
+    })    
 }
--- async-demo/promise.js
+++ async-demo/promise.js
@@ -2,7 +2,7 @@ const p = new Promise((resolve, reject) => {
     // Start some async work
     // ...
     setTimeout(() => {
-        // resolve(1);
+        resolve(1);
         reject(new Error('This is the error message'));
     }, 2000);    
 });
