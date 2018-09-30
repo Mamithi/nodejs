399de070c36ea85fd20385d3c1f24e3a0d9b46b0
Mamithi <19lawrence93@gmail.com>
2018-09-30 19:36:20 +0300

    Testing strings and arrays

---
+++ testing-demo/tests/lib.test.js
@@ -0,0 +1,34 @@
+const lib = require('../lib');
+
+describe('absolute', () => {
+    it('should return a positive number if input is positive', () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return a positive number if input is negative', () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return zero if input is zero', () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
+});
+
+describe('greet', () => {
+    it('should return the greeting message', () => {
+        const result = lib.greet('Lawrence');
+        // expect(result).toMatch(/Lawrence/);
+        expect(result).toContain('Lawrence');
+    });
+});
+
+describe('getCurrencies', () => {
+    it('should return supported currencies', () => {
+        const result = lib.getCurrencies();
+        expect(result).toEqual(expect.arrayContaining(['EUR', 'AUD', 'USD']));
+    });
+});
+
